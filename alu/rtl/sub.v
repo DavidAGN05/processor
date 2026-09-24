d88d761c074480253aea1f647990ecd1d0ea8777
david <dgarci48@jh.edu>
2026-09-24 14:08:01 -0400

    sub and block diagram

--- alu/rtl/sub.v
+++ alu/rtl/sub.v
@@ -1,9 +1,9 @@
 module sub (
-  input signed [8:0] a, b,
-  output [9:0] q);
+  input signed [7:0] a, b,
+  output [8:0] q);
   
-  wire [9:0] a_extended = $signed(a);
-  wire [9:0] b_extended = $signed(b);  
+  wire [8:0] a_extended = $signed(a);
+  wire [8:0] b_extended = $signed(b);
   
   assign q = a_extended - b_extended;
 endmodule
